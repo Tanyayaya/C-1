f95df48e06a666dc80fbc41d4687b8f7fecb7323
Tanyayaya <31395377+Tanyayaya@users.noreply.github.com>
2017-10-16 18:57:09 +0800

    Add files via upload

--- Win32Project1.cpp
+++ Win32Project1.cpp
@@ -1,53 +1,32 @@
-# include <iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-	//输入待排序数组
-	int a[10];
-	for (int i = 0; i < 10; i++) {
-		cin >> a[i];
+int main() {
+	char sentence[500]={0};
+	int num[500] = {0};
+	cin.getline(sentence, 500);
+	int numk = 1;
+	for (int i = 0; i < 500; i++) {
+		if (sentence[i] == ' '){
+			num[numk] = i;
+			numk++;}
+        if (sentence[i]=='.'){
+            num[numk] = i;
+            break;
+            }
 	}
-	//分为奇数偶数两个数组
-	int b[10], c[10];
-	int oddnum = 0, evenum = 0;
-	for (int i = 0; i < 10; i++) {
-		if (a[i]%2 == 0) {
-			b[evenum] = a[i];
-			evenum++;
-			}
-		else {
-			c[oddnum] = a[i];
-			oddnum++;
-		    }
-	}
+    int max = num[1];
+	int beforemax = -1;
+	int aftermax = num[1];
+	for (int i = 1; i <numk+1 ; i++) {
+            if (num[i + 1] - num[i] - 1 > max) {
+			   max = num[i + 1] - num[i] - 1;
+			   beforemax = num[i];
+			   aftermax = num[i + 1];
 
-	//用冒泡法进行排序
-	for (int j = 0; j < evenum; j++) {
-		for (int i = 0; i <(evenum-j-1); i++) {
-			if(b[i]>b[i+1]){
-				int k = b[i];
-				b[i] = b[i + 1];
-				b[i + 1] = k;
+        }
 
-			}
-		}
 	}
-	for (int j = 0; j < oddnum; j++) {
-		for (int i = 0; i <  (oddnum- j-1); i++) {
-			if (c[i]>c[i + 1]) {
-				int k = c[i];
-				c[i] = c[i + 1];
-				c[i + 1] = k;
-
-			}
-		}
+	for (int i = beforemax + 1; i < aftermax; i++) {
+		cout<<sentence[i];
 	}
-	//输出数组
-	for (int i = 0; i < oddnum; i++) {
-		cout << c[i];
-	}
-	for (int i = 0; i < evenum; i++) {
-		cout << b[i];
-	}
-	return 0;
 }
